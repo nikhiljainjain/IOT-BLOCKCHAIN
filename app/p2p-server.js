6a5d34ba55caa7f94ff3101da48638ad78197dfa
NIKHIL JAIN <nikhiljain.ashajain@gmail.com>
2019-10-15 23:56:03 +0530

    basic implemetation

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,16 +2,11 @@ const WebSocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-const MESSAGE_TYPE = {
-    chain: 'CHAIN',
-    transaction: 'TRANSACTION',
-    clear_transactions: 'CLEAR_TRANSACTIONS'
-};
 
 class P2pServer {
-    constructor(blockchain, transactionPool) {
+
+    constructor(blockchain) {
         this.blockchain = blockchain;
-        this.transactionPool = transactionPool;
         this.sockets = [];
     }
 
@@ -38,28 +33,13 @@ class P2pServer {
     }
 
     sendChain(socket) {
-        socket.send(JSON.stringify({ type: MESSAGE_TYPE.chain,chain: this.blockchain.chain }));
-    }
-
-    sendTransaction(socket, transaction) {
-        socket.send(JSON.stringify({ type: MESSAGE_TYPE.transaction, transaction }));
+        socket.send(JSON.stringify({ chain: this.blockchain.chain }));
     }
 
     messageHandler(socket) {
-        //console.log('messageHandler called');
         socket.on('message', message => {
             const data = JSON.parse(message);
-            switch (data.type) {
-                case MESSAGE_TYPE.chain:
-                    this.blockchain.replaceChain(data.chain);
-                    break;
-                case MESSAGE_TYPE.transaction:
-                    this.transactionPool.updateOrAddTransaction(data.transaction);
-                    break;
-                case MESSAGE_TYPE.clear_transactions:
-                    this.transactionPool.clear();
-                    break;
-            }
+            this.blockchain.replaceChain(data.chain);    
         });
     }
 
@@ -67,18 +47,6 @@ class P2pServer {
         this.sockets.forEach(socket => {
             this.sendChain(socket);
         });
-    }
-
-    broadcastTransaction(transaction) {
-        this.sockets.forEach(socket => {
-            this.sendTransaction(socket, transaction);
-        });
-    }
-
-    broadcastClearTransaction(){
-        this.sockets.forEach(s => s.send(JSON.stringify({
-            type: MESSAGE_TYPE.clear_transactions
-        })))
     }
 }
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,59 +3,20 @@ const router = express.Router();
 
 const P2pServer = require('../p2p-server');
 const Blockchain = require('../../blockchain');
-const Wallet = require('../../wallet');
-const TransactionPool = require('../../wallet/transaction-pool');
-const Miner = require('../miner');
 
 let bc = new Blockchain();
-let wallet = new Wallet();
-let tp = new TransactionPool();
-let p2pServer = new P2pServer(bc, tp);
-let miner = new Miner(bc, tp, wallet, p2pServer);
+let p2pServer = new P2pServer(bc);
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'NIKHIL$JAIN' });
+    res.render('index');
 });
 
+//it sends complete block chain
 router.get('/blocks', (req, res)=>{
-	//console.log(bc.chain);
 	res.json(bc.chain);
 });
 
-router.get('/transactions', (req, res)=>{
-	res.json(tp.transactions);
-});
-
-router.get('/public-key', (req, res)=>{
-	res.json({publicKey: wallet.publicKey});
-});
-
-wallet.balance = wallet.calculateBalance(bc);
-
-router.get('/balance', (req, res)=>{
-	res.json({balance : wallet.balance});
-});
-
-router.get('/mine-transactions', (req, res)=>{
-	const block = miner.mine(wallet);
-	console.log(`New block added: ${block}`);
-	res.status(302).redirect('/blocks');
-});
-
-router.get('/balance', (req, res)=>{
-    res.json({'Balance' : wallet.balance});
-});
-
-router.post('/transact', (req, res)=>{
-	const { recipient, amount } = req.body;
-	const transaction = wallet.createTransaction(recipient, amount, bc, tp);
-	if(typeof(transaction) === 'object'){
-		p2pServer.broadcastTransaction(transaction);
-		res.status(302).redirect('/transactions');
-	}else
-		res.json({msg: transaction});
-});
-
+//Getting data and mine to the blockchain
 router.post('/mine', (req, res)=>{
 	const nwBlock = bc.addBlock(req.body.data);
 	console.log(`New block added  ${nwBlock.toString()}`);
